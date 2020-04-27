96cba657d293101f7e37b2806996175650869327
IronL <280137563@qq.com>
2020-04-27 14:07:40 +0800

    chore: 添加sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const { src, dest, parallel, watch, series } = require("gulp");
 const ts = require("gulp-typescript");
+const sourcemaps = require("gulp-sourcemaps");
 
 function TypeScript() {
   return src("src/**/*.{tsx,ts}")
+    .pipe(sourcemaps.init())
     .pipe(ts.createProject("tsconfig.json")())
+    .pipe(sourcemaps.write("./"))
     .pipe(dest("dist/"));
 }
 
